e78d9a4bc30f63002374f2264aed38f4c98db4c4
Dmitry Ivanov <di.ivanov2018@gmail.com>
2023-10-30 00:41:44 +0300

    modified algoritm of reversing and render of component String

--- src/components/string/utils.ts
+++ src/components/string/utils.ts
@@ -7,3 +7,28 @@ export const swap = <T>(
   arr[firstIndex] = arr[secondIndex];
   arr[secondIndex] = temp;
 };
+
+export const getReversingSteps = (str: string): string[][] => {
+  const result: string[][] = [];
+  if (str === '') {
+    result.push([]);
+    return result
+  } else {
+    const letters = str.split('');
+    if (letters.length === 1) {
+      result.push(letters.slice());
+      return result;
+    } else {
+      result.push(letters.slice());
+      let start: number = 0;
+      let end: number = letters.length - 1;
+      while (start < end) {
+        swap(letters, start, end);
+        result.push(letters.slice());
+        start++;
+        end--;
+      }
+      return result;
+    }
+  }
+}
